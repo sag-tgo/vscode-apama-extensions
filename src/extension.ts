d2bdbbb47911e0ce0585c1d40dc1cfe5a8aec30d
sag-tgo <56433746+sag-tgo@users.noreply.github.com>
2020-03-30 17:49:06 +0100

    Added say hello command - created map of command IDs (string) to function.

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@
 
 import * as path from 'path';
 
-import { Disposable, ExtensionContext, workspace, debug, OutputChannel, window, tasks } from 'vscode';
+import * as vscode from 'vscode';
 
 import { ApamaEnvironment } from './apama_util/apamaenvironment';
 import { ApamaTaskProvider } from './apama_util/apamataskprovider';
@@ -12,30 +12,40 @@ import { ApamaProjectView } from './apama_project/apamaProjectView';
 //
 // client activation function, this is the entrypoint for the client
 //
-export function activate(context: ExtensionContext): void {
-	let commands: Disposable[] = [];
-	const logger = window.createOutputChannel('Apama Extension');
+export function activate(context: vscode.ExtensionContext): void {
+	let commands: vscode.Disposable[] = [];
+	const logger = vscode.window.createOutputChannel('Apama Extension');
 	logger.show();
 	logger.appendLine('Started EPL Extension');
 
 	let apamaEnv:ApamaEnvironment = new ApamaEnvironment(logger);
 	const taskprov = new ApamaTaskProvider(logger,apamaEnv);
-	context.subscriptions.push(tasks.registerTaskProvider( "apama" , taskprov ));
+	context.subscriptions.push(vscode.tasks.registerTaskProvider( "apama" , taskprov ));
 
 	const provider = new ApamaDebugConfigurationProvider(logger,apamaEnv);
-	context.subscriptions.push(debug.registerDebugConfigurationProvider('apama', provider));
+	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('apama', provider));
 	context.subscriptions.push(provider);
 
 	//this needs a workspace folder which under some circumstances can be undefined. 
 	//but we can ignore in that case and things shjould still work
-	if (workspace.workspaceFolders !== undefined) 
+	if (vscode.workspace.workspaceFolders !== undefined) 
 	{
-		const projView = new ApamaProjectView(apamaEnv, logger, workspace.workspaceFolders ,context);
+		const projView = new ApamaProjectView(apamaEnv, logger, vscode.workspace.workspaceFolders, context);
 	}
 
 	// Push the disposable to the context's subscriptions so that the 
 	// client can be deactivated on extension deactivation
 	commands.forEach(command => context.subscriptions.push(command));
+
+	const commands2 = new Map();
+	commands2.set("myExtension.sayHello", (name: string = "world") => {
+		console.log(`Hello ${name}!!!`);
+	});
+
+	for (let [command, commandHandler] of commands2) {
+		context.subscriptions.push(vscode.commands.registerCommand(command, commandHandler));
+	}
+
 }
 
 
