76a36efa3350a1799db39754b5fa7b1e79534cfe
CaribouJohn <john@heath.mx>
2020-03-31 12:43:36 +0100

    Added attempted startup for EPL Buddy

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,12 @@ import * as path from 'path';
 
 import { Disposable, ExtensionContext, workspace, debug, OutputChannel, window, tasks } from 'vscode';
 
+import {
+	LanguageClient, LanguageClientOptions, ServerOptions,
+	TransportKind, ForkOptions
+} from 'vscode-languageclient';
+
+
 import { ApamaEnvironment } from './apama_util/apamaenvironment';
 import { ApamaTaskProvider } from './apama_util/apamataskprovider';
 import { ApamaDebugConfigurationProvider } from './apama_debug/apamadebugconfig';
@@ -32,6 +38,50 @@ export function activate(context: ExtensionContext): void {
 	{
 		const projView = new ApamaProjectView(apamaEnv, logger, workspace.workspaceFolders ,context);
 	}
+
+
+	//---------------------------------
+	// Language server start-up and support
+	//---------------------------------
+
+		// This is the EPL Buddy application
+		
+		//let serverModule: string = context.asAbsolutePath(path.join('out', 'server.js'));
+		let serverModule: string = apamaEnv.getEplBuddyCmdline();
+		
+
+
+		// The debug options for the server
+		//let debugOptions: ForkOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
+		let debugOptions: ForkOptions = { execArgv: [] };
+	
+		// If the extension is launched in debug mode then the debug server options are used
+		// Otherwise the  normal ones are used
+		let serverOptions: ServerOptions = {
+			command: serverModule,
+			args: [],
+			options: {
+				shell: true
+			}
+		};
+	
+		// Options of the language client
+		let clientOptions: LanguageClientOptions = {
+			// Activate the server for epl files
+			documentSelector: [ 'epl' ],
+			synchronize: {
+				// Synchronize the section 'eplLanguageServer' of the settings to the server
+				configurationSection: 'eplLanguageServer',
+				// Notify the server about file changes to epl files contained in the workspace
+				// need to think about this
+				// fileEvents: workspace.createFileSystemWatcher('**/.epl')
+			}
+		};
+	
+		// Create the language client and start the client.
+		let langServer: Disposable = new LanguageClient('eplLanguageServer', 'Language Server', serverOptions, clientOptions).start();
+	
+
 
 	// Push the disposable to the context's subscriptions so that the 
 	// client can be deactivated on extension deactivation
