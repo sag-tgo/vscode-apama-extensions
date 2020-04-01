e4e04047bb8babf063ce6afff753ffad09f9b152
Heath <jhea@softwareag.com>
2020-04-01 16:41:15 +0100

    merge in langserver changes and c8y view plus sdk

---
+++ src/c8y/cumulocityView.ts
@@ -0,0 +1,118 @@
+import { window, commands, Disposable, workspace, OutputChannel, TreeDataProvider, EventEmitter, Event, TreeView, FileSystemWatcher, ExtensionContext, QuickPickItem, TextDocument, Uri, TreeItemCollapsibleState, TreeItem, WorkspaceFolder, RelativePattern, WorkspaceConfiguration } from 'vscode';
+import { ApamaProject, ApamaProjectWorkspace, ApamaTreeItem, BundleItem } from '../apama_project/apamaProject';
+import { ApamaEnvironment } from '../apama_util/apamaenvironment';
+import {Client, BasicAuth} from '@c8y/client';
+
+
+
+export class cumulocityView implements TreeDataProvider<string> {
+	private _onDidChangeTreeData: EventEmitter<string | undefined> = new EventEmitter<string | undefined>();
+	readonly onDidChangeTreeData: Event<string | undefined> = this._onDidChangeTreeData.event;
+
+	private treeView: TreeView<{}>;
+
+	//
+	// Added facilities for multiple workspaces - this will hopefully allow 
+	// ssh remote etc to work better later on, plus allows some extra organisational
+	// facilities....
+	constructor(private apamaEnv: ApamaEnvironment, private logger: OutputChannel, private context?: ExtensionContext) {
+		let subscriptions: Disposable[] = [];
+		
+		//project commands 
+		this.registerCommands();
+
+		//the component
+		this.treeView = window.createTreeView('c8y', { treeDataProvider: this });
+	}
+	processResponse(resp:any): void {
+		this.logger.appendLine("Status:" + resp.res.status + " " + resp.res.statusText);
+	}
+
+	processError(resp:any): void {
+
+		if( 'res' in resp )
+		{
+			this.logger.appendLine("Status:" + resp.res.status + " " + resp.res.statusText);
+		}
+		else
+		{
+			this.logger.appendLine("Status: Error " + resp.message);
+		}
+	}
+
+	registerCommands(): void {
+		if (this.context !== undefined) {
+			this.context.subscriptions.push.apply(this.context.subscriptions, [
+
+				//
+				// Create project 
+				//
+				commands.registerCommand('extension.c8y.login', async () => {
+					let config:WorkspaceConfiguration = workspace.getConfiguration('softwareag.c8y');
+
+					if( config ) {
+						let tenant:string = config.get('tenant',"");
+						let user:string = config.get('user',"");
+						//let userpw:string = config.get('password',"IfAtFirstYouDon'tSucceed2");
+						let password:string = "IfAtFirstYouDon'tSucceed2";
+						let baseurl:any = config.get('url',"");
+						this.logger.appendLine("Logging into c8y");
+
+						let x = new BasicAuth({
+							tenant,
+							user,
+							password
+						});
+
+						let client = new Client(x,baseurl);
+
+						try {
+							let y = await client.inventory.list();
+							let z = y.data;
+						}
+						catch (err) {
+							debugger;
+						}
+							
+				}}),
+
+			]);
+		}
+	}
+
+	//
+	// Trigger refresh of the tree
+	//
+	refresh(): void {
+		this._onDidChangeTreeData.fire();
+	}
+
+	//
+	// get the children of the current item (group or item)
+	// made this async so we can avoid race conditions on updates
+	//
+	async getChildren(item?: string | undefined): Promise<undefined | string[] > {
+
+		//if this is a bundle - then there are no children
+		return [];
+	}
+
+
+
+	//
+	// interface requirement
+	//
+	getTreeItem(element: BundleItem | ApamaProject | string): TreeItem {
+
+		//No string nodes in my tree so should never happen
+		if (typeof element === "string") {
+			this.logger.appendLine("ERROR ???? getTreeItem -- " + element.toString());
+			return new TreeItem(element, TreeItemCollapsibleState.None);
+		}
+
+		//should just be the element clicked on
+		return <TreeItem>element;
+	}
+}
+
+
--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,12 @@
 "use_strict";
 
-import * as path from 'path';
+import * as net from 'net';
 
 import { Disposable, ExtensionContext, workspace, debug, OutputChannel, window, tasks } from 'vscode';
 
 import {
 	LanguageClient, LanguageClientOptions, ServerOptions,
-	TransportKind, ForkOptions
+	TransportKind, ForkOptions, MessageTransports
 } from 'vscode-languageclient';
 
 
@@ -14,6 +14,7 @@ import { ApamaEnvironment } from './apama_util/apamaenvironment';
 import { ApamaTaskProvider } from './apama_util/apamataskprovider';
 import { ApamaDebugConfigurationProvider } from './apama_debug/apamadebugconfig';
 import { ApamaProjectView } from './apama_project/apamaProjectView';
+import { cumulocityView } from './c8y/cumulocityView';
 
 //
 // client activation function, this is the entrypoint for the client
@@ -39,6 +40,7 @@ export function activate(context: ExtensionContext): void {
 		const projView = new ApamaProjectView(apamaEnv, logger, workspace.workspaceFolders ,context);
 	}
 
+	const c8yView = new cumulocityView(apamaEnv, logger, context);
 
 	//---------------------------------
 	// Language server start-up and support
@@ -57,35 +59,45 @@ export function activate(context: ExtensionContext): void {
 		// Start the command in a shell - note that the current EPL buddy doesn't repond 
 		// so this will fail until we do have a working lang-server app
 		// https://github.com/Microsoft/vscode-languageserver-node/issues/358
-		let serverOptions: ServerOptions = {
-			command: serverModule,
-			args: [],
-			options: {
-				shell: true
-			}
-		};
-	
-		// Options of the language client
-		let clientOptions: LanguageClientOptions = {
-			// Activate the server for epl files
-			documentSelector: [ 'epl' ],
-			synchronize: {
-				// Synchronize the section 'eplLanguageServer' of the settings to the server
-				configurationSection: 'eplLanguageServer',
-				// Notify the server about file changes to epl files contained in the workspace
-				// need to think about this
-				// fileEvents: workspace.createFileSystemWatcher('**/.epl')
-			}
-		};
-	
-		// Create the language client and start the client.
-		let langServer: Disposable = new LanguageClient('eplLanguageServer', 'Language Server', serverOptions, clientOptions).start();
+
+
+		let langServer: Disposable = startLangServerTCP(12346).start();
 	
 
 
 	// Push the disposable to the context's subscriptions so that the 
 	// client can be deactivated on extension deactivation
 	commands.forEach(command => context.subscriptions.push(command));
+}
+
+
+function startLangServerTCP(addr: number): LanguageClient {
+	const serverOptions: ServerOptions = () => {
+	  return new Promise((resolve, reject) => {
+		const clientSocket = new net.Socket();
+		clientSocket.connect(12346, "10.60.84.57", () => {
+		  resolve({
+			reader: clientSocket,
+			writer: clientSocket,
+		  });
+		});
+	  });
+	};
+	
+	// Options of the language client
+	let clientOptions: LanguageClientOptions = {
+		// Activate the server for epl files
+		documentSelector: [ 'epl' ],
+		synchronize: {
+			// Synchronize the section 'eplLanguageServer' of the settings to the server
+			configurationSection: 'eplLanguageServer',
+			// Notify the server about file changes to epl files contained in the workspace
+			// need to think about this
+			// fileEvents: workspace.createFileSystemWatcher('**/.epl')
+		}
+	};
+
+	return new LanguageClient(`tcp lang server (port ${addr})`, serverOptions, clientOptions);
 }
 
 
